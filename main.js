ca57aa5fff4f8feff4e41a5e06a4ee34c47fd939
VenLk <81517322+VenLkStudio@users.noreply.github.com>
2024-06-07 01:56:04 +0400

    update

--- main.js
+++ main.js
@@ -37,3 +37,42 @@ searchInput.addEventListener("input", () => {
         notFoundMessage.style.display = "none";
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let slides = document.getElementsByClassName("slide");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 5000); // Change image every 5 seconds
+    }
+
+    document.querySelector(".prev").addEventListener("click", () => {
+        plusSlides(-1);
+    });
+
+    document.querySelector(".next").addEventListener("click", () => {
+        plusSlides(1);
+    });
+
+    function plusSlides(n) {
+        showSlide(slideIndex += n);
+    }
+
+    function showSlide(n) {
+        let slides = document.getElementsByClassName("slide");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+    }
+});
+
