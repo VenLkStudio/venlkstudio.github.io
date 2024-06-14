6997057b448b7cbe822c5fd5f27471b4f4c99edf
VenLk <81517322+VenLkStudio@users.noreply.github.com>
2024-06-14 15:26:23 +0300

    update

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
-const main = document.getElementById("main");
-const projectsPage = document.getElementById("projectsPage");
-const projectsList = document.getElementById("projects-list");
-const searchInput = document.getElementById("searchInput");
-const notFoundMessage = document.querySelector(".not-found");
-const projects = Array.from(document.querySelectorAll(".projects-page article"));
+const main = document.getElementById('main')
+const projectsPage = document.getElementById('projectsPage')
+const projectsList = document.getElementById('projects-list')
+const searchInput = document.getElementById('searchInput')
+const notFoundMessage = document.querySelector('.not-found')
+const projects = Array.from(document.querySelectorAll('.projects-page article'))
 
-document.getElementById("projects").addEventListener("click", () => {
-    main.style.opacity = "0";
-    projectsPage.style.opacity = "1";
-    projectsPage.style.transform = "translateY(0)";
-});
+document.getElementById('projects').addEventListener('click', () => {
+	main.style.opacity = '0'
+	projectsPage.style.opacity = '1'
+	projectsPage.style.transform = 'translateY(0)'
+})
 
-document.getElementById("home").addEventListener("click", () => {
-    main.style.opacity = "1";
-    projectsPage.style.opacity = "0";
-    projectsPage.style.transform = "translateY(100%)";
-});
+document.getElementById('home').addEventListener('click', () => {
+	main.style.opacity = '1'
+	projectsPage.style.opacity = '0'
+	projectsPage.style.transform = 'translateY(100%)'
+})
 
-searchInput.addEventListener("input", () => {
-    const searchTerm = searchInput.value.trim().toLowerCase();
+searchInput.addEventListener('input', () => {
+	const searchTerm = searchInput.value.trim().toLowerCase()
 
-    projects.forEach(project => {
-        const projectName = project.querySelector("h2").textContent.trim().toLowerCase();
-        if (projectName.includes(searchTerm)) {
-            project.style.display = "block";
-        } else {
-            project.style.display = "none";
-        }
-    });
+	projects.forEach(project => {
+		const projectName = project
+			.querySelector('h2')
+			.textContent.trim()
+			.toLowerCase()
+		if (projectName.includes(searchTerm)) {
+			project.style.display = 'block'
+		} else {
+			project.style.display = 'none'
+		}
+	})
 
-    const visibleProjects = projects.filter(project => project.style.display !== "none");
+	const visibleProjects = projects.filter(
+		project => project.style.display !== 'none'
+	)
 
-    if (visibleProjects.length === 0) {
-        notFoundMessage.style.display = "block";
-    } else {
-        notFoundMessage.style.display = "none";
-    }
-});
-
-
+	if (visibleProjects.length === 0) {
+		notFoundMessage.style.display = 'block'
+	} else {
+		notFoundMessage.style.display = 'none'
+	}
+})
